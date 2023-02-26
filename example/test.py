373a36c9dbb443267817b366d6ce45e5f5b32982
jinbeom, jeong <ppgt1225@naver.com>
2023-02-26 18:54:24 +0900

    fix can message parser

---
+++ example/test.py
@@ -0,0 +1,32 @@
+import can, time, threading
+import numpy as np
+from utils.can_msg_parser import MsgParser
+
+
+np.set_printoptions(suppress=True)
+bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
+
+
+def can_com():
+    print('waiting for can messages...')
+    for msg in bus:
+        can_id = msg.arbitration_id
+        can_payload = msg.data
+
+        can_msg_parser.get_under_utils_status(can_id, can_payload)
+        can_msg_parser.get_under_utils_data(can_id, can_payload)
+
+
+can_msg_parser = MsgParser()
+can_com_task = threading.Thread(target=can_com)
+can_com_task.daemon = True
+can_com_task.start()
+
+i = 0
+while True:
+    if input() == 'coord data':
+        print(i, can_msg_parser.read_variable('under_utils_coord_data'))
+
+    i += 1
+    time.sleep(0.5)
+
--- utils/can_msg_parser.py
+++ utils/can_msg_parser.py
@@ -1,6 +1,5 @@
 import can
 import numpy as np
-from sklearn.preprocessing import minmax_scale
 
 
 np.set_printoptions(suppress=True)
@@ -15,10 +14,13 @@ class MsgParser:
         self.__equipment_coord_frame_right__ = 0x18FF5011  # CD -> IC/SC
         self.__equipment_coord_frame_altitude__ = 0x18FF5012  # CD -> IC/SC
 
-        self.__automatic_control__: str = ''
-        self.__under_utils_data_transmit__: str = ''
+        self.__automatic_control__: str = 'none'
+        self.__under_utils_data_transmit__: str = 'none'
         self.__under_utils_data_number__: int = 0
-        self.__equipment_pos__: np.ndarray = np.zeros(5)  # gps_1_x, gps_1_y, gps_2_x, gps_2_y
+        self.__equipment_pos__: np.ndarray = np.zeros(5, dtype=np.uint32)  # gps_1_x, gps_1_y, gps_2_x, gps_2_y
+        self.__position_data__: np.ndarray = np.zeros(3, dtype=np.int32)  # x_pos, y_pos, z_pos
+
+        self.__class__.get_under_utils_data.called = False
 
     def get_automatic_control(self, arbitration_id: int, payload: []) -> str:
         if arbitration_id == self.__automatic_control_recv_frame__:
@@ -31,8 +33,15 @@ class MsgParser:
 
             return self.__automatic_control__
 
-    def get_under_utils_data(self, arbitration_id: int, payload: []):
+    def get_under_utils_status(self, arbitration_id: int, payload: []):
         if arbitration_id == self.__under_utils_data_frame__:
+            if payload[0] == 0:
+                self.__automatic_control__ = 'off'
+            elif payload[0] == 1:
+                self.__automatic_control__ = 'stand by'
+            elif payload[0] == 2:
+                self.__automatic_control__ = 'done'
+
             if payload[1] == 0:
                 self.__under_utils_data_transmit__ = 'none'
             elif payload[1] == 1:
@@ -44,26 +53,33 @@ class MsgParser:
 
             return self.__under_utils_data_transmit__, self.__under_utils_data_number__
 
-    def get_under_utils_info(self, arbitration_id: int, payload: []):
-        __under_utils_coord_id__ = np.zeros(self.__under_utils_data_number__)
-        __under_utils_depth_id__ = np.zeros(self.__under_utils_data_number__)
-        __under_utils_data_set__ = np.zeros((self.__under_utils_data_number__, 4))
+    def get_under_utils_data(self, arbitration_id: int, payload: []):
+        if not self.__class__.get_under_utils_data.called:
+            self.__under_utils_coord_id__ = np.zeros(self.__under_utils_data_number__, dtype=np.uint32)
+            self.__under_utils_depth_id__ = np.zeros(self.__under_utils_data_number__, dtype=np.uint32)
+            self.__under_utils_coord_data__ = np.zeros((self.__under_utils_data_number__, 2), dtype=np.uint32)
+            self.__under_utils_depth_data__ = np.zeros((self.__under_utils_data_number__, 2), dtype=np.float32)
+            self.__class__.get_under_utils_data.called = True
 
-        for i in range(self.__under_utils_data_number__):
-            __under_utils_coord_id__[i] = int('0x18FF20'+str(i)+'0')
+        else:
+            for i in range(self.__under_utils_data_number__):
+                self.__under_utils_coord_id__[i] = int('0x18FF20' + str(i + 1) + '0', base=16)
 
-        for i in range(self.__under_utils_data_number__):
-            __under_utils_depth_id__[i] = int('0x18FF201'+str(i))
+            for i in range(self.__under_utils_data_number__):
+                self.__under_utils_depth_id__[i] = int('0x18FF201' + str(i + 1), base=16)
 
-        if np.any(__under_utils_coord_id__ == arbitration_id):
-            __under_utils_data_set__[int(str(arbitration_id)[8]), 0:2] = np.array(int(payload[4:8], base=16),
-                                                                                  int(payload[0:4], base=16))
+            for coord_id in self.__under_utils_coord_id__:
+                if arbitration_id == coord_id:
+                    self.__under_utils_coord_data__[int(hex(arbitration_id)[8]) - 1, 0:2] = \
+                        np.array([int.from_bytes(payload[4:8], byteorder='little'),
+                                  int.from_bytes(payload[0:4], byteorder='little')], dtype=np.uint32)
 
-        if np.any(__under_utils_depth_id__ == arbitration_id):
-            __under_utils_data_set__[int(str(arbitration_id)[9]), 2:4] = np.array(int(payload[0:2], base=16),
-                                                                                  payload[2])
+            for depth_id in self.__under_utils_depth_id__:
+                if arbitration_id == depth_id:
+                    self.__under_utils_depth_data__[int(hex(arbitration_id)[9]), 2:4] = \
+                        np.array(int.from_bytes(payload[0:2], byteorder='little'), payload[2])
 
-        return __under_utils_data_set__
+        return self.__under_utils_coord_data__, self.__under_utils_depth_data__
 
     def get_equipment_pos(self, arbitration_id: int, payload: []):
         if arbitration_id == self.__equipment_coord_frame_left__:
@@ -77,19 +93,34 @@ class MsgParser:
 
             return self.__equipment_pos__
 
-    def create_estimated_position(self, control_flag: int = 0, state_flag: int = 0, x_pos: float = 0.0,
-                                y_pos: float = 0.0, z_pos: float = 0.0):
+    def create_estimated_position_can_msg(self, control_flag: int = 0, state_flag: int = 0, x_pos: float = 0.0,
+                                  y_pos: float = 0.0, z_pos: float = 0.0):
 
-        position_data = []
-        for data in [x_pos, y_pos, z_pos]:
-            position_data.append(int(data*100))
+        for i, data in enumerate([x_pos, y_pos, z_pos]):
+            self.__position_data__[i] = (int(data*100))
 
-        x_pos_msb, x_pos_lsb = position_data[0].to_bytes(2, byteorder='little')
-        y_pos_msb, y_pos_lsb = position_data[1].to_bytes(2, byteorder='little')
-        z_pos_msb, z_pos_lsb = position_data[2].to_bytes(2, byteorder='little')
+        x_pos_msb, x_pos_lsb = self.__equipment_pos__[0].to_bytes(2, byteorder='little')
+        y_pos_msb, y_pos_lsb = self.__position_data__[1].to_bytes(2, byteorder='little')
+        z_pos_msb, z_pos_lsb = self.__position_data__[2].to_bytes(2, byteorder='little')
         __send_data__ = [control_flag, state_flag, x_pos_msb, x_pos_lsb, y_pos_msb, y_pos_lsb, z_pos_msb, z_pos_lsb]
 
         send_msg = can.Message(arbitration_id=self.__automatic_control_send_frame__, data=__send_data__,
                                is_extended_id=True)
 
         return send_msg
+
+    def read_variable(self, parameter_name=''):
+        if parameter_name == 'automatic_control':
+            return self.__automatic_control__
+
+        elif parameter_name == 'under_utils_data_transmit':
+            return self.__under_utils_data_transmit__
+
+        elif parameter_name == 'under_utils_data_number':
+            return self.__under_utils_data_number__
+
+        elif parameter_name == 'equipment_pos':
+            return self.__equipment_pos__
+
+        elif parameter_name == 'under_utils_coord_data':
+            return self.__under_utils_coord_data__
