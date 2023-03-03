c94d2eb939befc13cf7e575ec93dc72a9b018eac
jinbeom, jeong <ppgt1225@naver.com>
2023-03-03 16:43:53 +0900

    update function of predict signal

---
+++ example/predict_test.py
@@ -0,0 +1,14 @@
+import time
+import numpy as np
+from utils.predcit_api import predict_control_quadratic
+from utils.tcp_lib import TCPClient
+
+
+tcp_handle = TCPClient(address='localhost', port=6340)
+tcp_handle.connect_to_server()
+
+while True:
+    signal = np.fromstring(tcp_handle.receive_msg(), dtype=np.float32, sep=' ')[0]
+    control_state, predict_signal = predict_control_quadratic(input_signal=signal, time_offset=0.3, kernel_size=10)
+    tcp_handle.send_msg(str(predict_signal))
+
--- utils/predcit_api.py
+++ utils/predcit_api.py
@@ -1,15 +1,21 @@
 import time
+import numpy as np
+from scipy.optimize import curve_fit
+
+np.set_printoptions(precision=3, suppress=True)
 
 
-t1 = 0
-signal_t1 = 0
+i: int = 0
+t0: float = 0.0
+t1: float = 0.0
+signal_t1: float = 0.0
 
 
-def predict_control(input_signal=0, time_offset=0.5, threshold_signal=0):
+def predict_control_linear(input_signal: float = 0, time_offset: float = 0.5,
+                           threshold_signal: float = 0) -> (int, float):
     t0 = time.time()
     signal_t0 = input_signal
-    global t1
-    global signal_t1
+    global t1, signal_t1
 
     derivative_time = t0 - t1
     derivative_signal = signal_t0-signal_t1
@@ -19,6 +25,49 @@ def predict_control(input_signal=0, time_offset=0.5, threshold_signal=0):
     control_state = 1 if predict_signal <= threshold_signal else 0
 
     signal_t1 = signal_t0
+    t1 = t0
+
+    return control_state, predict_signal
+
+
+kernel: np.ndarray
+time_scale: np.ndarray
+
+
+def predict_model(input_value, a, b, c):
+    return a*(input_value**2)+(b*input_value)+c
+
+
+def predict_control_quadratic(input_signal: float = 0, time_offset: float = 0.5,
+                              threshold_signal: float = 0, kernel_size: int = 5) -> (int, float):
+
+    global i, kernel, time_scale, t0, t1
+
+    if i == 0:
+        kernel = np.zeros(kernel_size, dtype=np.float32)
+        time_scale = np.linspace(0, kernel_size, kernel_size, endpoint=False)
+
+    t0 = time.time()
+    dt = t0-t1
+    time_arr = time_scale * dt
+
+    kernel = np.delete(kernel, kernel_size-1)
+    kernel = np.insert(kernel, 0, input_signal)
+    cal_kernel = np.flip(kernel)
+    p_opt = curve_fit(f=predict_model, xdata=time_arr, ydata=cal_kernel, method='dogbox')
+
+    time_offset = time_arr[kernel_size-1]+time_offset
+    predict_signal = (p_opt[0][0]*(time_offset**2))+(p_opt[0][1]*time_offset)+p_opt[0][2]
+
+    control_state = 1 if predict_signal <= threshold_signal else 0
+
+    '''
+    predict_signal = np.sum(np.multiply(p_opt[0][0], np.power(time_offset, 2)),
+                            np.multiply(p_opt[0][1], time_offset),
+                            p_opt[0][2])
+    '''
+
+    i += 1
     t1 = t0
 
     return control_state, predict_signal
