f83858e0ebc72ec16671992c0b55f3ab2b417592
jinbeom, jeong <ppgt1225@naver.com>
2023-03-03 16:48:36 +0900

    fix test app of predict signal

--- example/predict_test.py
+++ example/predict_test.py
@@ -9,6 +9,8 @@ tcp_handle.connect_to_server()
 
 while True:
     signal = np.fromstring(tcp_handle.receive_msg(), dtype=np.float32, sep=' ')[0]
-    control_state, predict_signal = predict_control_quadratic(input_signal=signal, time_offset=0.3, kernel_size=10)
-    tcp_handle.send_msg(str(predict_signal))
+    control_state, predict_signal = predict_control_quadratic(input_signal=signal, threshold_signal=1,
+                                                              time_offset=0.3, kernel_size=10)
+
+    tcp_handle.send_msg(str(control_state)+','+str(predict_signal))
 
